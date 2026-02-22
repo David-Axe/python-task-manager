e975adaf9727f6615c494a55e30a63e9793cf3b8
Davi Machado <davimachado.ep@gmail.com>
2026-02-22 17:44:46 -0300

    Create calc_gorjeta.py

---
+++ exercicios/dia1/calc_gorjeta.py
@@ -0,0 +1,6 @@
+#Peca o valor da conta e o percentual de gorjeta desejado. Mostre: valor da gorjeta, total com gorjeta, e valor por pessoa (pergunte quantas pessoas).
+valor_pessoa = float(80.00)
+print('Então vamos fechar a conta!')
+quantidade = float(input('Quantas pessoas? '))
+gorjeta = float(input('Quantos por cento deseja dar de gorjeta? '))
+
