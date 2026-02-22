d31401ff2ad8aaec48f9224209cf555b20741cc3
Davi Machado <davimachado.ep@gmail.com>
2026-02-22 18:32:45 -0300

    exercicio dia2 par_impar

---
+++ exercicios/dia2/par_impar.py
@@ -0,0 +1,6 @@
+#Peca um numero e diga se e par ou impar.
+numero = int(input('Digite algum número: '))
+if numero % 2 == 0:
+    print(f'O número {numero} é par!')
+else:
+    print(f'O número {numero} é ímpar!')
