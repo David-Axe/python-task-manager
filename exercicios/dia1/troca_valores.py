3ec40a80bf754a2edcb8f5fc401ea79cc6183ba2
Davi Machado <davimachado.ep@gmail.com>
2026-02-22 18:14:57 -0300

    exercicio dia 1 troca_valores

---
+++ exercicios/dia1/troca_valores.py
@@ -0,0 +1,5 @@
+#Crie duas variaveis a = 10 e b = 20. Troque os valores entre elas (a passa a valer 20, b passa a valer 10) SEM usar uma terceira variavel. Dica: Python permite!
+a, b = 10, 20
+print(a, b)
+a, b = b, a
+print(a, b)
