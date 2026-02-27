cadc6c1ce555fd4b4a53627212db694f43c4a130
Davi Machado <davimachado.ep@gmail.com>
2026-02-26 21:54:51 -0300

    Update analisador_texto.py

--- exercicios/dia5/analisador_texto.py
+++ exercicios/dia5/analisador_texto.py
@@ -6,7 +6,7 @@
 #Texto sem vogais
 
 #1. Programa deve receber informação (texto) do usuário;
-#2. Processar o valor recebido para transformar em saída texto com tudo maiúsculo (parâmetro upper) e outras saída texto com tudo minúsculo (parâmetro lower).
-#3. Processar novamente o valor primitivo para inverter a ordem dos caracteres (parâmetro texto [::-1])
-#4. Contabilizar as vogais (.count(a,e,i,o,u))
+#2. Processar o valor recebido para transformar em saída texto com tudo maiúsculo (função upper) e outras saída texto com tudo minúsculo (função lower).
+#3. Processar novamente o texto original para inverter a ordem dos caracteres (fatiamento texto [::-1])
+#4. Contabilizar as vogais (.count()) separadamente.
 #5. Substituir vogais por ('') usando .replace.
