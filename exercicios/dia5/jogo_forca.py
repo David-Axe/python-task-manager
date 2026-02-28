f684b17c1753223aeca98a9ab12df9889c3460e5
Davi Machado <davimachado.ep@gmail.com>
2026-02-28 11:05:37 -0300

    exercicio dia5

---
+++ exercicios/dia5/jogo_forca.py
@@ -0,0 +1,62 @@
+#Enunciado:
+#Implemente o jogo da forca:
+#Lista de palavras pre-definidas
+#Computador escolhe uma aleatoriamente
+#Jogador tem 6 tentativas
+#Mostre as letras acertadas e as erradas
+
+#Algoritmo:
+#1. Criar uma lista de palavras
+#2. Sortear uma palavra da lista
+#3. Criar variável com 6 tentativas
+#4. Criar lista vazia para letras erradas
+#5. mostrar palavra oculta (_ _ _ _ _)
+#6. enquanto tentativas > 0 e palavra não completas:
+    #7. pedir uma letra ao usuário
+    #8. se a letra está na palavra:
+        #9 mostrar a letra na posição correta
+    #10. senão:
+        #11. subtrair 1 tentativa e
+        #12. adicionar a letra na lista de erradas
+    #13. mostrar letras erradas
+    #14. mostrar tentativas restantes
+#15. se tentativas == 0:
+    #16. mostrar 'game over'
+#17. se palavra foi descoberta (sem '_'):
+    #18. mostrar 'você ganhou'
+
+#Código:
+
+
+#bibliotecas:
+import random
+
+palavras = ['elefante', 'cavalo', 'cruzeiro', 'canguru', 'Inglaterra', 'formiga', 'esmeralda', 'restaurante', 'alfaiate', 'marceneiro']
+
+#variáveis
+tentativas = 6
+letras_erradas = []
+palavra = random.choice(palavras)
+palavra_oculta = ['_' for _ in palavra]
+print(' '.join(palavra_oculta), flush=True)
+
+
+while tentativas > 0 and '_' not in palavra_oculta:
+    letra = input('Chute uma letra:')
+    if letra in palavra:
+        if letra in palavra_oculta:
+            print('Você já descobriu esta letra!', flush=True)
+        else:
+            for i in range(len(palavra)):
+                if palavra[i] == letra:
+                    palavra_oculta[i] = letra
+                    print(' '.join(palavra_oculta), flush=True)
+    else:
+        letras_erradas.append(letra)
+        tentativas -= 1
+        print(letras_erradas, flush=True)
+    print(f'Tentativas restantes: {tentativas}', flush=True)
+if tentativas == 0:
+    print('Game over!', flush=True)
+elif '_' not in palavra_oculta:
+    print('Você GANHOU!', flush=True)
