53a17e34057f4450eb6377c0a274d075f7b38d99
Davi Machado <davimachado.ep@gmail.com>
2026-04-28 21:48:41 -0300

    subtração temporaria

--- exercicios/backups/dia1/tipos_dados.py
+++
@@ -1,9 +0,0 @@
-#Crie um programa que pede ao usuario um valor e mostra:
-#O valor digitado
-#O tipo do valor
-#O valor convertido para int (se possivel)
-#O valor convertido para float (se possivel)
-valor = input('Digite algum valor(número): ')
-print(
-    f'Valor digitado: {valor} \nTipo de valor: {type(valor).__name__} \nValor convertido para int: {int(float(valor))} \nValor convertido para float: {float(valor)}'
-)
